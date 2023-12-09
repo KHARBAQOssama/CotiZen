4dc075e3958cde59e22a4f47406f7ee1b06bf0e8
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-09 19:30:10 +0100

    add a seeder for the user

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require("../api/components/users/models");
 require("dotenv").config();
 
 module.exports = () => {
@@ -12,5 +13,25 @@ module.exports = () => {
 
   db.once("open", async () => {
     console.log("Connected to MongoDB");
+    seedDefaultSyndic();
   });
 };
+
+async function seedDefaultSyndic() {
+  const existingUser = await User.findOne({
+    email: process.env.DEFAULT_SYNDIC_EMAIL,
+  });
+
+  if (!existingUser) {
+    let user = new User({
+      full_name: process.env.DEFAULT_SYNDIC_FULL_NAME,
+      email: process.env.DEFAULT_SYNDIC_EMAIL,
+      password: process.env.DEFAULT_SYNDIC_PASSWORD,
+    });
+    let insertedUser = await user.save();
+    console.log("User inserted successfully!");
+    return insertedUser;
+  } else {
+    console.log("User already exists!");
+  }
+}
