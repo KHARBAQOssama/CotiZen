9cbd16286783ba775580dc13cbf51832c062f57f
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-11 16:36:14 +0100

    create a new route for the update apartment satus function

--- Server/api/components/appartements/routes/index.js
+++ Server/api/components/appartements/routes/index.js
@@ -7,5 +7,6 @@ router.post("/", authenticated, AppartmentService.create);
 router.patch("/:appartement", authenticated, AppartmentService.update);
 router.delete("/:appartement", authenticated, AppartmentService.deleteA);
 router.get("/:appartement", authenticated, AppartmentService.show);
+router.patch("/status/:appartement", authenticated, AppartmentService.updateApartmentStatus);
 
 module.exports = router;
