493a5ff34517da7501cc5ff3266ad0f941229f49
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-19 08:59:24 +0100

    create a modal

--- Client/src/components/organisms/AddModal.jsx
+++ Client/src/components/organisms/AddModal.jsx
@@ -1,54 +1,115 @@
 import { useState } from "react";
-import OwnerInfoForm from "../molecules/OwnerInfoForm";
-import AppartementInfo1 from "../molecules/AppartementInfo1";
-import AppartementInfo2 from "../molecules/AppartementInfo2";
+import utils from "../../utils";
+import {
+  createApartment,
+  clearMessage,
+} from "../../redux/actions/apartmentActions";
+import { useDispatch, useSelector } from "react-redux";
 
-const AddModal = ({ open,toggleOpen }) => {
+const AddModal = ({ open, setOpen }) => {
+  const dispatch = useDispatch();
+  const message = useSelector((state) => state.apartments.message);
   const [step, setStep] = useState(1);
-  const [appartementOwner, setAppartementOwner] = useState({
-    email: "",
-    full_name: "",
-    phone_number: "",
+  const [error, setError] = useState("");
+  const [apartment, setApartment] = useState({
+    number: "",
+    address: "",
+    monthlyPayment: "",
+    status: "available",
   });
+  const handleChange = (e) => {
+    setApartment({ ...apartment, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = () => {
+    let errors = utils.apartmentValidation(apartment);
+    if (errors.length) {
+      setError(errors[0]);
+      return;
+    } else {
+      setError("");
+    }
+    dispatch(createApartment(apartment));
+  };
   return (
     <>
       {open && (
-        <div className="w-[100vw] h-[100vh] flex absolute top-0 left-0 bg-opacity-5 bg-black">
+        <div className="w-[100%] h-[100vh] flex absolute top-0 left-0 bg-opacity-20 bg-black">
           <form
             action=""
-            className="flex-col flex gap-5 m-auto relative rounded-tr-md bg-white p-5 px-8 rounded-2xl w-[30%]"
+            className="flex-col flex gap-5 m-auto relative rounded-tr-md bg-white p-5 px-8 rounded-2xl w-[30%] min-w-[300px]"
             onSubmit={(e) => {
               e.preventDefault();
+              handleSubmit();
             }}
           >
-            <h1 className="text-3xl py-2">Add Appartement</h1>
-            <button onClick={toggleOpen} className="absolute right-8 text-xl top-8 text-gray-300 hover:text-black">x</button>
-            {step == 1 && <OwnerInfoForm/>}
-            {step == 2 && <AppartementInfo1/>}
-            {step == 3 && <AppartementInfo2/>}
-            <div className="flex">
-              {step > 1 && (
+            {(!message || !message.content) && (
+              <>
+                <h1 className="text-3xl py-2">Add Apartment</h1>
                 <button
-                  onClick={() => setStep(step - 1)}
-                  className="bg-gray-50 text-gray-500 px-5 p-1 border-2 border-gray-50 rounded-md hover:border-black hover:text-black"
+                  onClick={() => setOpen(!open)}
+                  className="absolute right-8 text-xl top-8 text-gray-300 hover:text-black"
                 >
-                  prev
+                  <i className="uil uil-times"></i>
                 </button>
-              )}
-              {step != 3 && (
-                <button
-                  onClick={() => setStep(step + 1)}
-                  className=" bg-purple-600 text-white ms-auto px-5 p-1 border-2 border-purple-600 rounded-md hover:bg-purple-700"
-                >
-                  next
-                </button>
-              )}
-              {step == 3 && (
+                <div className="w-full gap-5 flex flex-col">
+                  {error && (
+                    <div className="w-full rounded-md text-start bg-red-200 px-3 py-2 flex justify-between">
+                      <h3 className=" text-red-600 font-semibold ">{error}</h3>
+                      <button
+                        className="text-red-600"
+                        onClick={() => setError("")}
+                      >
+                        <i className="uil uil-times"></i>
+                      </button>
+                    </div>
+                  )}
+
+                  <input
+                    type="text"
+                    className="bg-gray-100 p-2 py-3 w-full rounded-md font-light"
+                    placeholder="Apartment Code"
+                    name="number"
+                    value={apartment.number}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <textarea
+                    className="bg-gray-100 p-2 py-3 w-full rounded-md font-light"
+                    placeholder="Apartment Address"
+                    name="address"
+                    value={apartment.address}
+                    onChange={(e) => handleChange(e)}
+                  ></textarea>
+                  <input
+                    type="number"
+                    className="bg-gray-100 p-2 py-3 w-full rounded-md font-light"
+                    placeholder="Apartment Monthly Payment"
+                    name="monthlyPayment"
+                    value={apartment.monthlyPayment}
+                    onChange={(e) => handleChange(e)}
+                  />
+                </div>
+
                 <button className=" bg-purple-600 text-white ms-auto px-5 p-1 border-2 border-purple-600 rounded-md hover:bg-purple-700">
                   Submit
                 </button>
-              )}
-            </div>
+              </>
+            )}
+            {message && message.content && (
+              <div className="flex flex-col gap-5">
+                <h3 className="text-center text-xl font-bold text-green-500">
+                  {message.content}
+                </h3>
+                <button
+                  onClick={() => {
+                    setOpen(!open);
+                    dispatch(clearMessage());
+                  }}
+                  className="bg-green-500 text-white w-max py-2 px-12 m-auto rounded-md"
+                >
+                  close
+                </button>
+              </div>
+            )}
           </form>
         </div>
       )}
