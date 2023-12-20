3f136c9db3dc2e48605d1064a78b418e9d23df44
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-20 15:15:05 +0100

    modify the invoice schema

--- Server/api/components/invoices/services/InvoiceService.js
+++ Server/api/components/invoices/services/InvoiceService.js
@@ -1,71 +1,13 @@
-// const getPaymentsForInvoice = async (invoiceId) => {
-//   try {
-//     const payments = await Payment.find({ invoice: invoiceId });
-//     return payments;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 const Invoice = require("../models");
-const Apartment = require("../../appartements/models");
-const getInvoiceWithResident = async (req,res) => {
-  const {invoice : invoiceId} = req.params
-  try {
-    const invoice = await Invoice.findById(invoiceId).exec();
-    if (!invoice) {
-      throw new Error("Invoice not found");
-    }
 
-    const apartment = await Apartment.findOne({
-      invoices: invoiceId,
-      "residentsHistory.startDate": { $lte: invoice.month },
-      $or: [
-        { "residentsHistory.endDate": null },
-        { "residentsHistory.endDate": { $gte: invoice.month } },
-      ],
-    }).exec();
-
-    if (!apartment) {
-      throw new Error("Apartment not found for the given invoice");
-    }
-
-    const resident = apartment.residentsHistory.find((resident) => {
-      const startDate = new Date(resident.startDate + "-01"); // Append the day "01" to the start date
-      const endDate = resident.endDate
-        ? new Date(resident.endDate + "-01")
-        : null; // Append the day "01" to the end date
-      const invoiceMonth = new Date(invoice.month + "-01"); // Append the day "01" to the invoice month
-
-      return startDate <= invoiceMonth && (!endDate || endDate >= invoiceMonth);
-    });
-
-    if (!resident) {
-      throw new Error("Resident not found for the given invoice");
-    }
-
-    return {
-      invoice: {
-        _id: invoice._id,
-        month: invoice.month,
-        amount: invoice.amount,
-        amount_paid: invoice.amount_paid,
-        status: invoice.status,
-      },
-      resident: {
-        _id: resident._id,
-        name: resident.name,
-        email: resident.email,
-        nationalId: resident.nationalId,
-        phoneNumber: resident.phoneNumber,
-        startDate: resident.startDate,
-        endDate: resident.endDate,
-      },
-    };
-  } catch (error) {
-    return res.status(404).json({error:error.message})
-  }
-};
-
+const getAllInvoices = async (req,res) =>{
+  const invoices = await Invoice.find()
+    .populate({
+      path: "apartment",
+    })
+    .populate({ path: "payments" });
+  res.status(200).json({invoices})
+}
 module.exports = {
-  getInvoiceWithResident,
+  getAllInvoices,
 };
