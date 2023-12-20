9c7f77c299d619fd5efe36d973308580268a08c7
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-20 15:08:09 +0100

    implement the auth midleware

--- Client/src/middlewares/Auth.jsx
+++ Client/src/middlewares/Auth.jsx
@@ -1,14 +1,15 @@
 import { createElement, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const RequireAuth = ({children}) => { 
-  const isAuthenticated = true;
+  const authenticated = useSelector((state) => state.users.authenticated);
   const navigate = useNavigate(); 
   useEffect(()=>{
-    if (!isAuthenticated) {
+    if (!authenticated) {
       navigate('/login')
     }
-  },[])
+  },[authenticated])
   return (children);
 };
 
