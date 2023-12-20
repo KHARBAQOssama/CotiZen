f41a3bc4e6f65ddf715c961c361565711353ee0d
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-20 15:04:30 +0100

    modify the sidebar component

--- Client/src/components/organisms/Sidebar.jsx
+++ Client/src/components/organisms/Sidebar.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import AddModal from "./AddModal";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/actions/userActions";
 const Sidebar = ({openSidebar,setOpenModal}) => {
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const [active, setActive] = useState();
+  const user = useSelector((state) => state.users.user);
+  const message = useSelector((state) => state.users.message);
   const location = useLocation();
   const navigate = useNavigate();
   const removeAdminPath = (inputString) => {
@@ -11,16 +16,21 @@ const Sidebar = ({openSidebar,setOpenModal}) => {
     const resultWithout = resultWithoutSlash.replace(/\/admin/, "");
     return resultWithout;
   };
+  const handleLogOut = () =>{
+    dispatch(logout());
+  }
   useEffect(() => {
     const activeTab = removeAdminPath(location.pathname);
     setActive(activeTab);
   }, []);
-
+  useEffect(()=>{
+    
+  })
   return (
       <aside
         className={`h-[100vh] lg:w-[275px] lg:relative transition-all ${
           openSidebar
-            ? `w-[100vw] absolute top-0 left-0 z-40`
+            ? `w-[100vw] absolute top-0 left-0 z-30`
             : "w-0 overflow-hidden"
         }`}
       >
@@ -49,6 +59,36 @@ const Sidebar = ({openSidebar,setOpenModal}) => {
             >
               <i className="uil uil-building"></i>
               Apartments
+            </li>
+            <li
+              onClick={() => {
+                navigate("/admin/payments");
+                setActive("/payments");
+              }}
+              className={`${active == "/payments" ? "border-r-4 border-purple-600" : ""} w-full flex gap-3 items-center p-3 text-xl hover:bg-purple-100`}
+            >
+              <i className="uil uil-invoice"></i>
+              Payments
+            </li>
+            <li
+              onClick={() => {
+                navigate("/admin/invoices");
+                setActive("/invoices");
+              }}
+              className={`${active == "/invoices" ? "border-r-4 border-purple-600" : ""} w-full flex gap-3 items-center p-3 text-xl hover:bg-purple-100`}
+            >
+              <i className="uil uil-invoice"></i>
+              Invoices
+            </li>
+            <li
+              
+              className={`rounded-xl w-max m-auto bg-yellow-300 mt-8 flex gap-3 items-center p-3 text-xl hover:bg-purple-100`}
+            >
+              <i className="uil uil-user"></i>
+              {user.full_name}
+              <button onClick={handleLogOut} className="bg-white p-2 rounded-3xl">
+               <i className="uil uil-sign-out-alt"></i>
+              </button>
             </li>
           </ul>
           <button className="absolute bottom-8 left-[50%] translate-x-[-50%] bg-purple-600 text-white rounded-xl font-semibold p-3" onClick={()=>setOpenModal(true)}>Add Apartment</button>
