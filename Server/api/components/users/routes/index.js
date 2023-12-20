762027dbcb02c0428def670a959c2c2222e9459a
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-20 15:15:45 +0100

    add logout function

--- Server/api/components/users/routes/index.js
+++ Server/api/components/users/routes/index.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 
 router.post('/auth/login', AuthServices.login);
 router.get('/auth/me',authenticated, AuthServices.me);
+router.get('/auth/logout',authenticated, AuthServices.logout);
 
 module.exports = router
