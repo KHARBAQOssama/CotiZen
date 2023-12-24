d0d4543a7dc8c615c24e320a13865638fb5bf9e2
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-24 11:23:04 +0100

    change  the pdf look ui

--- Client/src/components/pages/InvoicePDF.jsx
+++ Client/src/components/pages/InvoicePDF.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Document,
   Page,
@@ -38,41 +38,44 @@ const InvoicePDF = ({ invoice }) => {
       fontSize: 18,
       textAlign: "right",
       marginTop: 20,
-      marginRight : 20,
+      marginRight: 20,
       fontWeight: "bold",
     },
   });
+  console.log(invoice);
   return (
     <PDFViewer width={"100%"} height={500}>
       <Document>
-      <Page size="A4" style={styles.page}>
+        <Page size="A4" style={styles.page}>
           <Text style={styles.header}>Invoice Details</Text>
           <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Invoice ID:  </Text>
+            <Text style={styles.sectionTitle}>Invoice ID: </Text>
             <Text style={styles.sectionContent}>{invoice._id}</Text>
           </View>
           <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Month:  </Text>
+            <Text style={styles.sectionTitle}>Month: </Text>
             <Text style={styles.sectionContent}>{invoice.month}</Text>
           </View>
           <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Total Amount:  </Text>
+            <Text style={styles.sectionTitle}>Total Amount: </Text>
             <Text style={styles.sectionContent}>${invoice.amount}</Text>
           </View>
           <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Amount Paid:  </Text>
+            <Text style={styles.sectionTitle}>Amount Paid: </Text>
             <Text style={styles.sectionContent}>${invoice.amount_paid}</Text>
           </View>
           <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Payment Status:  </Text>
+            <Text style={styles.sectionTitle}>Payment Status: </Text>
             <Text style={styles.sectionContent}>{invoice.status}</Text>
           </View>
           <View style={styles.section}>
-            <Text style={styles.sectionTitle}>Apartment Number:  </Text>
-            <Text style={styles.sectionContent}>{invoice.apartment.number}</Text>
+            <Text style={styles.sectionTitle}>Apartment Number: </Text>
+            <Text style={styles.sectionContent}>
+              {invoice.apartment.number}
+            </Text>
           </View>
           <View style={styles.totalAmount}>
-            <Text>Total Paid:   ${invoice.amount_paid}</Text>
+            <Text>Total Paid: ${invoice.amount_paid}</Text>
           </View>
         </Page>
       </Document>
